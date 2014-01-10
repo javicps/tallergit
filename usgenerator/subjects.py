9dea60c739476f3483c17a5527b3b0ab55696905
Pedro Rivas Montero <rmdrobs@gmail.com>
2014-01-10 20:10:39 +0100

    new subjects added

--- usgenerator/subjects.py
+++ usgenerator/subjects.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 subjects = (u'cliente',
+			u'desarrollador',
+			u'barrendero',
+			u'recepcionista',
+			u'analista',
+			u'repartidor de sobres',
+			u'mono borracho',
+			u'paciente',
+			u'apu nahasapeemapetilon',
+			u'afilador',
             u'consultor')

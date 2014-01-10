eb2b1ab6c488e19d4e337d08877ade609470541d
Christian Forcada Vitalla <chris.forcadavitalla@gmail.com>
2014-01-10 20:28:57 +0100

    nueva rama, más verbos

--- usgenerator/verbs.py
+++ usgenerator/verbs.py
@@ -12,4 +12,7 @@ verbs = (u'modificar',
          u'ver',
          u'imprimir',
          u'enviar por correo',
-         u'mejorar')
+         u'mejorar',
+         u'escuchar',
+         u'robar'
+         u'crackear')

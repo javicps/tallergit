4e2e22983461b521cb7a801e515f58f13ca79b3e
Alfonso Romay <aromay@gmail.com>
2014-01-10 20:09:48 +0100

    Cambiando verbos en python

--- usgenerator/verbs.py
+++ usgenerator/verbs.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 verbs = (u'modificar',
-         u'mejorar')
+         u'mejorar',
+         u'ridiculizar',
+         u'institucionalizar',
+         u'hackear')

2d1e7f5cf4a24c09b6835a2f7a6f483b3f4bddee
jmartinter <jmartinter@gmail.com>
2015-02-13 19:47:54 +0100

    Añadidos más verbos

--- usgenerator/verbs.py
+++ usgenerator/verbs.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 verbs = (u'modificar',
-         u'mejorar')
+         u'mejorar',
+	 u'arreglar',
+	 u'construir',
+	 u'optimizar',
+	 u'molar',
+	 u'commitear',
+	 u'estropear',
+	 u'bailar'
+)

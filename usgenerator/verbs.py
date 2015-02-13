877ff77087bf5b8ddd9175baae70c1b732fbdca3
Alberto Portoles <alberto@albertoportoles.es>
2015-02-13 20:08:59 +0100

    Cambios verb2

--- usgenerator/verbs.py
+++ usgenerator/verbs.py
@@ -3,6 +3,10 @@
 verbs = (u'modificar',
          u'eliminar',
          u'empeorar',
+         u'colorear',         
+         u'cambiar',
          u'colocar',
+         u'dirigir',
+         u'almentar',
          u'aplicar',
          u'mejorar')

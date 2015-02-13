43c8f48aefccb7bc55b6c91b9e6f4e10169bd7ba
Alberto Portoles <alberto@albertoportoles.es>
2015-02-13 19:59:03 +0100

    Cambios verbs

--- usgenerator/verbs.py
+++ usgenerator/verbs.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 verbs = (u'modificar',
+         u'eliminar',
+         u'empeorar',
+         u'colocar',
+         u'aplicar',
          u'mejorar')

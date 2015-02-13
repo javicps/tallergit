02fef62992073e60a52b5e9efc54d11d960e09c6
Jorge Ferrer <ferrer.jorge@gmail.com>
2015-02-13 19:48:58 +0100

    Actualizado el objects.py

--- usgenerator/objects.py
+++ usgenerator/objects.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 objects = (u'el flujo de navegación',
-           u'la autenticación')
+           u'la autenticación',
+           u'el estado de la nación',
+           u'el clima en Toronto',
+           u'todo',
+           u'el código fuente'
+           )

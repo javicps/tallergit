4ac70e926ed91061191757b863894897a9d28c3b
Jorge Ferrer <ferrer.jorge@gmail.com>
2015-02-13 20:12:33 +0100

    Un montón de chorradas para el objects.py

--- usgenerator/objects.py
+++ usgenerator/objects.py
@@ -5,5 +5,14 @@ objects = (u'el flujo de navegación',
            u'el estado de la nación',
            u'el clima en Toronto',
            u'todo',
-           u'el código fuente'
+           u'el código fuente',
+           u'las esquinas redondeadas',
+           u'los equipos de desarrollo',
+           u'el ziritione',
+           u'las maletas',
+           u'el pajarillo que me mira por la ventana de la oficina',
+           u'la mano que mece la cuna',
+           u'los números de línea primos',
+           u'los clientes',
+           u'unos cuantos insultos'
            )

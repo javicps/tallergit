7b8fd4799472323f1d73ebd9018e1f37dd1a8766
jon marturet <jmarturet@gmail.com>
2015-02-13 19:48:07 +0100

    mas cambios

--- usgenerator/environments.py
+++ usgenerator/environments.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 environments = (u'en la web de Renfe',
-                u'en la base de datos corporativa')
+                u'en la base de datos corporativa',
+                u'en la web del congreso',
+                u'en la web de la ESA')

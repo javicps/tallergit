77ced220276f5485e78335e28f99d62c015b04b3
Razvan Puscas <rpuscas94@gmail.com>
2015-03-26 20:14:36 +0100

    More enviroments

--- usgenerator/environments.py
+++ usgenerator/environments.py
@@ -7,6 +7,8 @@ environments = (u'en la web de Renfe',
                 u'en la base de datos del FBI',
                 u'en la web de Twitter',
                 u'en Facebook',
-                u'en la NASA')
+                u'en la NASA'
+                u'en el patio de mi casa',
+                u'en mi movil')
 
 

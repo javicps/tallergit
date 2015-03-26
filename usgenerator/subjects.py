c27ea02c64f7075711a4b9a0d8a583fadb019763
Víctor Guerrero <vguerrero@vgsystems.es>
2015-03-26 20:17:47 +0100

    Added some subjetcs

--- usgenerator/subjects.py
+++ usgenerator/subjects.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 subjects = (u'cliente',
-            u'Product Owner',
-            u'Scrum Master',
-            u'Developer',
-            u'Tester',
-            u'QA Manager',
-            u'consultor',
-			u'multimillonario derrochador',
-			u'amante de git',
-            u'superhéroe mierder',
-			u'desconocido',
-			u'sombrerero loco',
-			u'primo de un colega'
-            u'compañero',
-                  u'tocapelotas'
-                  u'útil'
-                  u'inútil'
-                  u'polaco')
+  u'Product Owner',
+  u'Scrum Master',
+  u'Developer',
+  u'Tester',
+  u'QA Manager',
+  u'consultor',
+  u'multimillonario derrochador',
+  u'amante de git',
+  u'superhéroe mierder',
+  u'desconocido',
+  u'sombrerero loco',
+  u'primo de un colega'
+  u'compañero',
+  u'tocapelotas'
+  u'útil'
+  u'inútil'
+  u'el puto amo'
+  u'el Rey de España'
+  u'polaco')
